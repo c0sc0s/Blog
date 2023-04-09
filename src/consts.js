a56c27cccbc9f45b8955cbd6c2f733fef59728f3
c0sc0s <yuwenjieshuai@gmail.com>
2023-04-09 21:10:00 +0800

    init

--- src/consts.js
+++ src/consts.js
@@ -1,11 +1,11 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `Cosine`;
+export const SITE_DESCRIPTION = 'Cosine\'s Blog';
+export const SITE_EMAIL = 'yuwenjieshuai@gmail.com'
+export const SITE_NAME = 'cos02.top';
+export const SITE_URL = "https://cos02.top";
 export const SITE_LANG = "zh-CN";
 
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "Austin";
+export const SITE_AUTHOR = "Cosine";
---
+++ src/pages/about.md
@@ -0,0 +1,17 @@
+---
+layout: '../layouts/MarkdownPost.astro'
+title: '草木皆宾'
+pubDate: 2023-04-09
+description: '热爱每一株小草，感谢每一束阳光'
+cover:
+    url: 'https://www.apple.com.cn/newsroom/images/values/environment/Apple-Earth-Day-India-mangrove-Alibaug-canoe_Full-Bleed-Image.jpg.large_2x.jpg'
+    square: 'https://www.apple.com.cn/newsroom/images/product/apps/standard/Apple-Freeform-hero_big.jpg.large_2x.jpg'
+    alt: 'cover'
+tags: ["cosine"] 
+theme: 'light'
+featured: true
+---
+
+![HomePod 汇集了多项 Apple 创新技术、Siri 智能和智能家居功能，为用户带来前所未有的聆听体验 | wide](https://www.apple.com.cn/newsroom/images/values/environment/Apple-Earth-Day-India-mangrove-Alibaug-canoe_Full-Bleed-Image.jpg.large_2x.jpg)
+
+
